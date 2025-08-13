47e2bd72b17295a8ae8cc961ff105fa468eef1e3
csy10001 <fuitshine@gmail.com>
2025-08-13 17:20:26 +0900

    팩토리얼 추가 완료

---
+++ src/main/java/step4/factorial.java
@@ -0,0 +1,16 @@
+package step4;
+
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        System.out.print("숫자를 입력해주세요 : ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int answer = 1;
+        for(int i = 1; i <= number; i++){
+            answer *= i;
+        }
+        System.out.println(answer);
+    }
+}
